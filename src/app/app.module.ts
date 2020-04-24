f32fe76270500ae9ee20cd41fb25c008dabf9015
Adriano Fernandes <afernandes130@hotmail.com>
2020-04-24 09:31:15 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CamelCasePipe } from './camel-case.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CamelCasePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
